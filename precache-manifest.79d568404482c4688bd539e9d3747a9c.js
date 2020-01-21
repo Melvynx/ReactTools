7d4a67f138271c6257a656ae63b36d1e0b5cf5c7
Melvyn Malherbe <melvyn.malherbe@qoqa.com>
2020-01-21 09:15:12 +0100

    Updates

---
+++ precache-manifest.79d568404482c4688bd539e9d3747a9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a009a995fdd99e44eeebdf79d92cabf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2f1b82d48a4db5cf6f42",
+    "url": "/static/css/main.1bf4e5f6.chunk.css"
+  },
+  {
+    "revision": "835f8b4eb5c694d2a814",
+    "url": "/static/js/2.0df0e1f8.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/static/js/2.0df0e1f8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2f1b82d48a4db5cf6f42",
+    "url": "/static/js/main.1b5542b4.chunk.js"
+  },
+  {
+    "revision": "da6d9570104af6bff2fb",
+    "url": "/static/js/runtime-main.6bda29f8.js"
+  }
+]);
