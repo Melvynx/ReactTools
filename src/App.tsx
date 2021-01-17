748a84352fe8528c5f857689b37694ea1c028705
Melvynx <melvyn.malherbe@qoqa.com>
2021-01-17 17:25:05 +0100

    update package and add baseroot

--- src/App.tsx
+++ src/App.tsx
@@ -1,67 +1,67 @@
-import React from "react";
+import React from 'react';
 
 import {
   CssBaseline,
   ThemeProvider,
   createMuiTheme,
   Box,
-  responsiveFontSizes
-} from "@material-ui/core";
-import { Switch, HashRouter as Router, Route } from "react-router-dom";
-import RandomSentence from "./page/RandomSentence";
-import HomePage from "./page/HomePage";
-import BaseCalculator from "./page/BaseCalculator";
-import GitHubPages from "./page/GitHubPages";
-import Footer from "./components/App/Footer";
-import Forum from "./page/Forum";
+  responsiveFontSizes,
+} from '@material-ui/core';
+import { Switch, HashRouter as Router, Route } from 'react-router-dom';
+import RandomSentence from './page/RandomSentence';
+import HomePage from './page/HomePage';
+import BaseCalculator from './page/BaseCalculator';
+import GitHubPages from './page/GitHubPages';
+import Footer from './components/App/Footer';
+import Forum from './page/Forum';
 
 let theme = createMuiTheme({
   palette: {
-    type: "dark",
+    type: 'dark',
     primary: {
-      main: "#bb87fa",
-      contrastText: "#000"
+      main: '#bb87fa',
+      contrastText: '#000',
     },
     secondary: {
-      main: "#1ddac6",
-      contrastText: "#fff"
+      main: '#1ddac6',
+      contrastText: '#fff',
     },
     error: {
-      light: "##ef9a9a",
-      main: "#ce667a",
-      dark: "#af0025",
-      contrastText: "#7e7e7e"
+      light: '##ef9a9a',
+      main: '#ce667a',
+      dark: '#af0025',
+      contrastText: '#7e7e7e',
     },
     background: {
-      default: "#1d1d1d",
-      paper: "#2c2c2c"
-    }
+      default: '#1d1d1d',
+      paper: '#2c2c2c',
+    },
   },
   typography: {
     subtitle1: {
-      fontStyle: "italic"
+      fontStyle: 'italic',
     },
     fontFamily: [
-      "-apple-system",
-      "BlinkMacSystemFont",
-      "Segoe UI",
-      "Roboto",
-      "Oxygen",
-      "Ubuntu",
-      "Cantarell",
-      "Open Sans",
-      "Helvetica Neue",
-      "sans-serif"
-    ].join(",")
+      '-apple-system',
+      'BlinkMacSystemFont',
+      'Segoe UI',
+      'Roboto',
+      'Oxygen',
+      'Ubuntu',
+      'Cantarell',
+      'Open Sans',
+      'Helvetica Neue',
+      'sans-serif',
+    ].join(','),
   },
   spacing: 8,
   overrides: {
     MuiButton: {
       text: {
-        borderRadius: 2
-      }
-    }
-  }
+        borderRadius: 2,
+      },
+    },
+  },
 });
 theme = responsiveFontSizes(theme);
 
@@ -70,7 +70,7 @@ export default function App() {
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <Box pb={34}>
-        <Router basename="">
+        <Router basename={process.env.PUBLIC_URL}>
           <Switch>
             <Route path="/randomWord">
               <RandomSentence />
